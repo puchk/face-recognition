ab4461dcca04e0ceb152a98b41d352dccabb8f65
puchk <kpuchalski@gmail.com>
2018-05-22 13:01:29 -0700

    changes register, sign in, sign up depending on page

--- src/App.js
+++ src/App.js
@@ -48,7 +48,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -82,6 +83,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route})
   }
 
@@ -89,7 +95,7 @@ class App extends Component {
     return (
       <div className="App">
         <Particles params={particleOptions} className='particles'/>
-        <Navigation onRouteChange={this.onRouteChange}/>
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home'
           ? <div>
               <Logo />
