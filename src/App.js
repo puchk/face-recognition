4fa35520ee3a70ab8215025845fb26c7ff361312
puchk <kpuchalski@gmail.com>
2018-05-24 15:21:07 -0700

    fetches json data from backend

--- src/App.js
+++ src/App.js
@@ -53,6 +53,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
   faceLocation = (data) => {
     const face =  data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
