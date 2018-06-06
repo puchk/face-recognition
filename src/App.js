e57792e546e68b1d243adf85b7dca0b125b74452
puchk <kpuchalski@gmail.com>
2018-06-05 17:14:03 -0700

    moved clarifai to back end

--- src/App.js
+++ src/App.js
@@ -7,13 +7,8 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 
 import './App.css';
-
-const app = new Clarifai.App({
- apiKey: 'ec6cbdadc3b14481a4fd94d91b9c7ae5'
-});
 
 const particleOptions = {
   particles: {
@@ -96,7 +91,14 @@ class App extends Component {
 
   onSubmitButton = () => {
     this.setState({imageUrl: this.state.input});
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              input: this.state.input
+            })
+          })
+    .then(response => response.json())
     .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
