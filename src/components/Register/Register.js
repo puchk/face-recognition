2dece2c64eca48a1bc47a5028f1a47315bb16aa5
puchk <kpuchalski@gmail.com>
2018-06-07 23:34:58 -0700

    updates with heroku

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-  	fetch('http://localhost:3000/register', {
+  	fetch('https://quiet-atoll-96440.herokuapp.com/register', {
   		method: 'post',
   		headers: {'Content-Type': 'application/json'},
   		body: JSON.stringify({
